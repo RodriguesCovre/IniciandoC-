0a2c62509504596c1dd531d29a0a670cd35b073d
Patrick <patrickcovrerodrigues@gmail.com>
2024-03-21 23:28:39 -0300

    Console Write e WriteLine

---
+++ Main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program{
+    static void Main(string[] args){
+        Console.Write("Ola, mundo \n");
+        Console.Write("Hello, Word \n");
+        Console.WriteLine("Ola, mundo");
+        Console.WriteLine("Hello, Word");
+    }
+}
