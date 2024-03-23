5eddfd77d7d844e3e5ae722102573724d3ced070
Patrick <patrickcovrerodrigues@gmail.com>
2024-03-23 19:02:59 -0300

    Conversao de String para int

--- conversaoDeDados.cs
+++ conversaoDeDados.cs
@@ -2,8 +2,12 @@ using System;
 
 class Conversao{
     static void Main(){
-        float n1 = 20.5f;
-        int n2 = (int)n1;
+        float n4 = 20.5f;
+        int n2 = (int)n4;
         Console.WriteLine(n2);
+
+        string t1 = "3";
+        int n1 = Convert.ToInt32(t1);
+        Console.WriteLine(n1);
     }
 }
