acdad976e31078755d827ec12ee440ccf7bf28b5
Patrick <patrickcovrerodrigues@gmail.com>
2024-03-24 00:28:41 -0300

    aula12

---
+++ tratamentoString.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Tratamento{
+    static void Main(){
+        string frase = "Hello, Word!";        
+        //Console.WriteLine(frase.ToUpper());
+        Console.WriteLine(frase.ToLower());
+    }
+}
