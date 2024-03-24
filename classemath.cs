d45acaf55792de43d150f3f2ae828a2c1388de23
Patrick <patrickcovrerodrigues@gmail.com>
2024-03-24 00:58:37 -0300

    max e minimo

--- classemath.cs
+++ classemath.cs
@@ -8,5 +8,8 @@ class Funcao{
         Console.WriteLine(Math.Round(3.4)); //Arredonda o mais proximo
         Console.WriteLine(Math.Ceiling(3.4)); //Arredonda a cima
         Console.WriteLine(Math.Floor(3.9)); //Arredonda a baixo
+
+        Console.WriteLine(Math.Max(3,7)); //Mostra o maior
+        Console.WriteLine(Math.Min(3, 7)); //Mostra o menor
     }
 }
