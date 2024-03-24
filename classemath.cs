0f5993792462d8919aab01a4d3e9c4c0c1fb64eb
Patrick <patrickcovrerodrigues@gmail.com>
2024-03-24 00:56:21 -0300

    aula15

---
+++ classemath.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Funcao{
+    static void Main(){
+        Console.WriteLine(Math.Pow(2,2)); //Potencia
+        Console.WriteLine(Math.Sqrt(9)); //Raiz Quadrada
+
+        Console.WriteLine(Math.Round(3.4)); //Arredonda o mais proximo
+        Console.WriteLine(Math.Ceiling(3.4)); //Arredonda a cima
+        Console.WriteLine(Math.Floor(3.9)); //Arredonda a baixo
+    }
+}
