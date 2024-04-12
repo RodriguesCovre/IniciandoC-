f626cf4ad01bc7e0cb332443412b2bf65fec438e
Patrick <patrickcovrerodrigues@gmail.com>
2024-04-11 23:02:48 -0300

    aula de array

---
+++ array.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Principal{
+    static void Main(){
+        int[] Vetor1 = new int[3]{10,20,30};
+        
+        for(int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(Vetor1[i]);
+        }
+    }
+}
